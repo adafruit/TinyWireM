11e64ceda56b94b2a318b53830a40b258e51bd95
Eric Lee <saintgimp@hotmail.com>
2014-01-01 19:10:46 -0800

    Add pin defines for ATTiny44/84

--- USI_TWI_Master.h
+++ USI_TWI_Master.h
@@ -79,6 +79,16 @@
     #define PIN_USI_SCL         PINB2
 #endif
 
+#if defined(__AVR_ATtiny84__) | defined(__AVR_ATtiny44__)
+    #  define DDR_USI           DDRA
+    #  define PORT_USI          PORTA
+    #  define PIN_USI           PINA
+    #  define PORT_USI_SDA      PORTA6
+    #  define PORT_USI_SCL      PORTA4
+    #  define PIN_USI_SDA       PINA6
+    #  define PIN_USI_SCL       PINA4
+#endif
+
 #if defined(__AVR_AT90Tiny2313__) | defined(__AVR_ATtiny2313__)
     #define DDR_USI             DDRB
     #define PORT_USI            PORTB
