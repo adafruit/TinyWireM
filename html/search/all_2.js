d3b903342581ccaba5f8978fcb4ee31531552681
Doxygen CI <ci-arduino@invalid>
2021-12-21 21:39:20 +0000

    Deploy docs to GitHub Pages from commit e7f4d4865d
    Commit: e7f4d4865d717b3231a108d23add3ff76dee87fd
    GitHub Actions run: 1608777013

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['end',['end',['../class_u_s_i___t_w_i.html#a018ff68cf83eb3963582dff2faff785b',1,'USI_TWI']]],
   ['endtransmission',['endTransmission',['../class_u_s_i___t_w_i.html#a305bf41e222299639d5f76e3a55daddc',1,'USI_TWI::endTransmission()'],['../class_u_s_i___t_w_i.html#a55d27ea8e1f29d4670775c213fad028c',1,'USI_TWI::endTransmission(uint8_t)']]],
   ['errorstate',['errorState',['../union_u_s_i___t_w_i__state.html#ab6b1ad0ab46bba6493866fe581869db4',1,'USI_TWI_state']]]
 ];
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['end',['end',['../class_u_s_i___t_w_i.html#a018ff68cf83eb3963582dff2faff785b',1,'USI_TWI']]],
   ['endtransmission',['endTransmission',['../class_u_s_i___t_w_i.html#a305bf41e222299639d5f76e3a55daddc',1,'USI_TWI::endTransmission()'],['../class_u_s_i___t_w_i.html#a55d27ea8e1f29d4670775c213fad028c',1,'USI_TWI::endTransmission(uint8_t)']]]
 ];
